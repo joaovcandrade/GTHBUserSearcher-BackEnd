5a5f10a1f16b89abac0b8aeb19d838271b30d2c7
João Vitor <jvcostaandrade7399@gmail.com>
2019-09-12 08:40:03 -0300

    fix bugs

--- Controller/search.js
+++ Controller/search.js
@@ -4,8 +4,9 @@ module.exports = {
 
     async name(req, res){
         const {name} = req.headers;
-
-        response = await axios.get(`https://api.github.com/search/users?q=${name}`); 
+        //console.log(name);
+        response = await axios.get(`https://api.github.com/search/users?q=${name}`);
+        
         return res.status(200).json(response.data.items);
 
     }
